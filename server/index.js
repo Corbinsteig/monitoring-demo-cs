efd89806c8082cd37b3aadf101871a07f639b6d7
Corbin Steig <corsteig1@gmail.com>
2022-04-28 11:36:25 -0700

    second commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'c2958f4214244812b4b465c101a49ee2',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"))

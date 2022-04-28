724fdaa4d273186450c4c1cd3edd4f6b1d5d32b6
Corbin Steig <corsteig1@gmail.com>
2022-04-28 11:41:47 -0700

    third commit

--- server/index.js
+++ server/index.js
@@ -15,7 +15,21 @@ rollbar.log('Hello world!')
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully.')
+})
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
 
+    students.push(name)
+    rollbar.log('Student added successfully', {author: 'Corbin', type: 'manual entry'})
+    res.status(200).send(students)
+})
 const port = process.env.PORT || 4545
+app.use(rollbar.errorHandler())
+
+app.use(express.json())
 
 app.listen(port, () => console.log (`Take us to warp ${port}!`))
